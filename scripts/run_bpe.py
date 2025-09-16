12c1902caa4993b23bd951f02786d632b3f7d123
tehboy <nathan.boy@gmail.com>
2025-09-16 00:49:55 +0000

    Update run_bpe script to use token utils save logic.

--- scripts/run_bpe.py
+++ scripts/run_bpe.py
@@ -1,22 +1,32 @@
+import logging
 import pathlib
 import time
 import cProfile
 from cs336_basics import bpe
+from cs336_basics.token_utils import save_vocab_and_merges
 import argparse
 import pickle
 import sys
 
 
 def main():
+    logging.basicConfig(
+        level=logging.INFO,                # Set log level
+        format="%(asctime)s %(levelname)s %(message)s",  # Set log format
+        filename="run_bpe.txt",              # Log to a file (optional)
+        filemode="w"                       # Overwrite file (optional)
+    )
     parser = argparse.ArgumentParser(description="Run BPE on input file.")
     parser.add_argument("input_path", type=str, help="Path to input text file")
     parser.add_argument("--vocab_size", type=int, default=500, help="Vocabulary size")
     parser.add_argument(
         "--special_tokens", type=str, nargs="+", default=["<|endoftext|>"], help="Special tokens"
     )
-    parser.add_argument("--debug", action="store_true", help="Write out human readable versions.")
     parser.add_argument(
         "--profile", action="store_true", help="Profile the bpe tokenization process."
+    )
+    parser.add_argument(
+        "--dry_run", action="store_true", help="Do not save vocab and merges to disk."
     )
     args = parser.parse_args()
 
@@ -29,21 +39,11 @@ def main():
         vocab_size=args.vocab_size,
         special_tokens=args.special_tokens,
     )
-    vocab_pickle_path = input_path.with_suffix(input_path.suffix + ".vocab")
-    merge_list_pickle_path = input_path.with_suffix(input_path.suffix + ".merge_list")
-    with open(vocab_pickle_path, "wb") as f:
-        pickle.dump(vocab, f)
-    with open(merge_list_pickle_path, "wb") as f:
-        pickle.dump(merge_list, f)
-    if args.debug:
-        vocab_file_path = input_path.with_suffix(input_path.suffix + ".vocab.txt")
-        with open(vocab_file_path, "w", encoding="utf-8") as vocab_file:
-            for token, bytes in vocab.items():
-                vocab_file.write(f"{token} {bytes}\n")
-        merge_list_file_path = input_path.with_suffix(input_path.suffix + ".merge_list.txt")
-        with open(merge_list_file_path, "w", encoding="utf-8") as merge_file:
-            for merge in merge_list:
-                merge_file.write(f"{merge[0] + merge[1]}\n")
+
+    if not args.dry_run:
+        vocab_path = input_path.with_suffix(input_path.suffix + ".vocab")
+        merges_path = input_path.with_suffix(input_path.suffix + ".merges")
+        save_vocab_and_merges(vocab, merge_list, vocab_path=vocab_path, merges_path=merges_path)
 
 
 if __name__ == "__main__":
