2571759aa7821d04453a282a8ef00e60a287cf8a
tehboy <nathan.boy@gmail.com>
2025-10-03 04:28:04 +0000

    Fixed script errors.

--- scripts/run_ablations.sh
+++ scripts/run_ablations.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-pushd /home/nathan_boy/cs336g-a1-basics
+# Get the directory of the script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# Go to the project root
+pushd "$SCRIPT_DIR/.."
+
 uv run scripts/run_model.py --model_file conf/TinyStories.yaml --ablate_norms
-mv ../data/TinyStoriesV2-GPT4-train.txt.best.checkpoint mv ../data/TinyStoriesV2-GPT4-train.txt.ablate_norms.checkpoint
+mv ../data/TinyStoriesV2-GPT4-train.txt.best.checkpoint ../data/TinyStoriesV2-GPT4-train.txt.ablate_norms.checkpoint
 
 uv run scripts/run_model.py --model_file conf/TinyStories.yaml --ablate_position_embeddings
-mv ../data/TinyStoriesV2-GPT4-train.txt.best.checkpoint mv ../data/TinyStoriesV2-GPT4-train.txt.ablate_position_embeddings.checkpoint
+mv ../data/TinyStoriesV2-GPT4-train.txt.best.checkpoint ../data/TinyStoriesV2-GPT4-train.txt.ablate_position_embeddings.checkpoint
 
 uv run scripts/run_model.py --model_file conf/TinyStories.yaml --use_silu --dff 2048
-mv ../data/TinyStoriesV2-GPT4-train.txt.best.checkpoint mv ../data/TinyStoriesV2-GPT4-train.txt.use_silu.checkpoint
+mv ../data/TinyStoriesV2-GPT4-train.txt.best.checkpoint ../data/TinyStoriesV2-GPT4-train.txt.use_silu.checkpoint
 
 uv run scripts/run_model.py --model_file conf/owt.yaml
 
