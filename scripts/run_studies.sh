2081369eac0706670c8b97e27633a481dfc4cff5
tehboy <nathan.boy@gmail.com>
2025-10-03 04:44:27 +0000

    Fix up run_studies script.

--- scripts/run_studies.sh
+++ scripts/run_studies.sh
@@ -11,9 +11,14 @@ mv ../data/TinyStoriesV2-GPT4-train.txt.best.checkpoint ../data/TinyStoriesV2-GP
 uv run scripts/run_model.py --model_file conf/TinyStories.yaml --ablate_position_embeddings
 mv ../data/TinyStoriesV2-GPT4-train.txt.best.checkpoint ../data/TinyStoriesV2-GPT4-train.txt.ablate_position_embeddings.checkpoint
 
-uv run scripts/run_model.py --model_file conf/TinyStories.yaml --use_silu --dff 2048
+uv run scripts/run_model.py --model_file conf/TinyStories.yaml --use_silu --d_ff 2048
 mv ../data/TinyStoriesV2-GPT4-train.txt.best.checkpoint ../data/TinyStoriesV2-GPT4-train.txt.use_silu.checkpoint
 
 uv run scripts/run_model.py --model_file conf/owt.yaml
+
+uv run scripts/run_model.py --model_file conf/TinyStories.yaml --max_lr=0.01 --min_lr=0.001
+uv run scripts/run_model.py --model_file conf/TinyStories.yaml --max_lr=0.005 --min_lr=0.0005
+uv run scripts/run_model.py --model_file conf/TinyStories.yaml --max_lr=0.0001 --min_lr=0.00001
+uv run scripts/run_model.py --model_file conf/TinyStories.yaml --max_lr=0.05 --min_lr=0.00001
 
 popd
